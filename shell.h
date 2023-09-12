38c724fc107c04d0727ac196f72d53f6844693b4
Janecanal <janecanal04@gmail.com>
2023-09-12 04:50:39 -0700

    arranged

--- shell.h
+++ shell.h
@@ -26,26 +26,24 @@ int _strncmp(const char *s1, const char *s2, int n);
 int _atoi(char *s);
 
 
+/*** Function prototypes for main functions****/
 int _launch(char **args);
 char **_checks_path(char **args, int *flag);
-int alloc_errors1_(char *buf);
-int alloc_errors_(char *buf, char *dest_path);
-
 char *_getenv_(const char *n);
 char **_splits_paths(char *commd, char *copy_commd);
 void sigint_handler(int signa);
-int _getc(void);
-
 char **splits_txts(char *commd);
-ssize_t _getline_(char **line,
-		size_t *n, __attribute__((unused))FILE * stream);
 int exec_work_builtin(char **args, char *input);
 char *read_commd_(void);
-int shell_exit(__attribute__((unused)) char **args, char *input);
+int _setenv(char *name, char *val);
+
+/** allocation of errors**/
+int alloc_errors1_(char *buf);
+int alloc_errors_(char *buf, char *dest_path);
+int allocs_errors(char *buffer);
 
 /* memory allocation */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int allocs_errors(char *buffer);
 
 /**
   * _help - the help function
@@ -65,15 +63,19 @@ int _help(__attribute__((unused)) char **args,
 int _env(__attribute__((unused)) char **args,
 	__attribute__((unused)) char *input);
 
-int _setenv(char *name, char *val);
+
+/** builtins**/
 int shell_cd(char **args, __attribute__((unused)) char *input);
 int shell_exit(__attribute__((unused)) char **args, char *input);
 
+/**customised getline function**/
 ssize_t _getline_(char **line,
 		size_t *n, __attribute__((unused))FILE * stream);
 
+
 int _putchar(char c);
 void _puts(char *str);
+int _getc(void);
 
 /* interact_loop */
 void interact_loop(void);
